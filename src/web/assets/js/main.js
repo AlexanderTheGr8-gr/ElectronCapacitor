7d0c167a5814ee4beb39aef368d4201c46f76fe4
Alexander Dimitriadis <alex_necromancer@yahoo.gr>
2025-05-09 06:37:35 +0300

    Initial Repo - Electron/Capacitor+

---
+++ src/web/assets/js/main.js
@@ -0,0 +1,9 @@
+// const setClock = () => {
+//     var clock = document.querySelector('.clock');
+//     const date = new Date();
+//     const hours = String(date.getHours()).padStart(2, '0');
+//     const minutes = String(date.getMinutes()).padStart(2, '0');
+//     const seconds = String(date.getSeconds()).padStart(2, '0');
+//     clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+// }
+// setInterval(setClock, 1000);
